5d04d2493d7ffc6ec9101cef6843d3506190039d
HuangLvPeng <318377072@qq.com>
2019-10-02 09:44:31 +0800

    1.界面搭建

--- StudentManagement/StudentManagement/Form1.Designer.cs
+++ StudentManagement/StudentManagement/Form1.Designer.cs
@@ -28,15 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("节点0");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("节点1");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("节点2");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("节点3");
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("学生列表");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("学生管理", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("班级列表");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("班主任列表");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("班级管理", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("教师列表");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("老师管理", new System.Windows.Forms.TreeNode[] {
+            treeNode6});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.tabCtrl_Content = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,42 +58,73 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
-            this.splitContainer1.Size = new System.Drawing.Size(843, 610);
-            this.splitContainer1.SplitterDistance = 281;
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabCtrl_Content);
+            this.splitContainer1.Size = new System.Drawing.Size(889, 610);
+            this.splitContainer1.SplitterDistance = 296;
             this.splitContainer1.TabIndex = 0;
             // 
-            // treeView1
+            // treeView
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            treeNode1.Name = "节点0";
-            treeNode1.Text = "节点0";
-            treeNode2.Name = "节点1";
-            treeNode2.Text = "节点1";
-            treeNode3.Name = "节点2";
-            treeNode3.Text = "节点2";
-            treeNode4.Name = "节点3";
-            treeNode4.Text = "节点3";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            treeNode1.Name = "StudentsList";
+            treeNode1.Text = "学生列表";
+            treeNode2.Name = "StudentsMgr";
+            treeNode2.Text = "学生管理";
+            treeNode3.Name = "ClassList";
+            treeNode3.Text = "班级列表";
+            treeNode4.Name = "HeadTeacherList";
+            treeNode4.Text = "班主任列表";
+            treeNode5.Name = "ClassMgr";
+            treeNode5.Text = "班级管理";
+            treeNode6.Name = "TeacherList";
+            treeNode6.Text = "教师列表";
+            treeNode7.Name = "TeacherMgr";
+            treeNode7.Text = "老师管理";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode2,
-            treeNode3,
-            treeNode4});
-            this.treeView1.Size = new System.Drawing.Size(281, 610);
-            this.treeView1.TabIndex = 0;
+            treeNode5,
+            treeNode7});
+            this.treeView.Size = new System.Drawing.Size(296, 610);
+            this.treeView.TabIndex = 0;
+            this.treeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TreeView_NodeMouseClick);
+            // 
+            // tabCtrl_Content
+            // 
+            this.tabCtrl_Content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabCtrl_Content.Location = new System.Drawing.Point(0, 0);
+            this.tabCtrl_Content.Name = "tabCtrl_Content";
+            this.tabCtrl_Content.SelectedIndex = 0;
+            this.tabCtrl_Content.Size = new System.Drawing.Size(589, 610);
+            this.tabCtrl_Content.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 584);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加节点";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 610);
+            this.ClientSize = new System.Drawing.Size(889, 610);
             this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -93,7 +134,9 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TabControl tabCtrl_Content;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- StudentManagement/StudentManagement/Form1.cs
+++ StudentManagement/StudentManagement/Form1.cs
@@ -15,6 +15,35 @@ namespace StudentManagement
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private List<TabPage> tabPages = new List<TabPage>();
+
+        /// <summary>
+        /// 树节点被鼠标单击时触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //|| 或者    && 并且
+            if(e.Node.Name== "StudentsList"|| e.Node.Name == "ClassList" || e.Node.Name == "HeadTeacherList"|| e.Node.Name == "TeacherList")
+            {
+                if (!tabPages.Exists(x => x.Text == e.Node.Text))
+                {
+                    TabPage tabPage = new TabPage();
+                    tabPage.Text = e.Node.Text;
+                    this.tabCtrl_Content.TabPages.Add(tabPage);
+                    tabPages.Add(tabPage);
+                }
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //点击后向树节点添加一个任节点。
         }
     }
 }
